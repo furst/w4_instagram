b1e7a41fbe7655409b0b35aedf62c6027445ea2b
Andreas Fürst <andreas.furst@gmail.com>
2014-05-08 20:36:31 +0200

    fixed jsonp

--- js/admin.js
+++ js/admin.js
@@ -10,24 +10,27 @@ jQuery(document).ready(function() {
 	jQuery('.user-search').on('click', function(e) {
 		userCon.empty();
 		var query = jQuery('.query').val();
-		var url = 'https://api.instagram.com/v1/users/search?';
-		
-		jQuery.get(url, {q: query, access_token: accessToken })
-			.done(function(data) {
-				jQuery.each(data.data, function(index, data) {
-					var row = jQuery('<div class="user-row"></div>');
-					var p = jQuery('<p></p>').text(data.full_name + ', ' + data.username + ', ' + data.id);
-					var a = jQuery('<a href="#"></a>').text('Add').data('id', data.id).addClass('user-link');
-					row.append(p)
-					row.append(a);
-					userCon.append(row);
-				});
+		var url = 'https://api.instagram.com/v1/users/search?q=' + query + '&access_token=' + accessToken + '&callback=?';
 
-				addUser();
-			})
-			.fail(function() {
-    			userCon.append(jQuery('<p></p>').text('Ett fel inträffade'));
-  			});
+		userCon.text('Loading...');
+
+		jQuery.getJSON(url, function(data) {
+			userCon.empty();
+			jQuery.each(data.data, function(index, data) {
+				var row = jQuery('<div class="user-row"></div>');
+				var p = jQuery('<p></p>').text(data.full_name + ', ' + data.username + ', ' + data.id);
+				var a = jQuery('<a href="#"></a>').text('Add').data('id', data.id).addClass('user-link');
+				row.append(p)
+				row.append(a);
+				userCon.append(row);
+			});
+
+			addUser();
+		})
+  		.fail(function() {
+    		userCon.empty();
+    		userCon.append(jQuery('<p></p>').text('Ett fel inträffade'));
+  		});
 
 		e.preventDefault();
 	});
@@ -49,24 +52,27 @@ jQuery(document).ready(function() {
 		locationCon.empty();
 		var latQuery = jQuery('.lat-query').val();
 		var lngQuery = jQuery('.lng-query').val();
-		var url = 'https://api.instagram.com/v1/locations/search?';
-		
-		jQuery.get(url, {lat: latQuery, lng: lngQuery, access_token: accessToken })
-			.done(function(data) {
-				jQuery.each(data.data, function(index, data) {
-					var row = jQuery('<div class="user-row"></div>');
-					var p = jQuery('<p></p>').text(data.id + ', ' + data.latitude + ', ' + data.longitude + ', ' + data.name);
-					var a = jQuery('<a href="#"></a>').text('Add').data('id', data.id).addClass('location-link');
-					row.append(p)
-					row.append(a);
-					locationCon.append(row);
-				});
+		var url = 'https://api.instagram.com/v1/locations/search?lat=' + latQuery + '&lng=' + lngQuery + '&access_token=' + accessToken + '&callback=?';
 
-				addLocation();
-			})
-			.fail(function() {
-    			locationCon.append(jQuery('<p></p>').text('Ett fel inträffade'));
-  			});
+		locationCon.text('Loading...');
+
+		jQuery.getJSON(url, function(data) {
+			locationCon.empty();
+			jQuery.each(data.data, function(index, data) {
+				var row = jQuery('<div class="user-row"></div>');
+				var p = jQuery('<p></p>').text(data.id + ', ' + data.latitude + ', ' + data.longitude + ', ' + data.name);
+				var a = jQuery('<a href="#"></a>').text('Add').data('id', data.id).addClass('location-link');
+				row.append(p)
+				row.append(a);
+				locationCon.append(row);
+			});
+
+			addLocation();
+		})
+  		.fail(function() {
+    		locationCon.empty();
+    		locationCon.append(jQuery('<p></p>').text('Ett fel inträffade'));
+  		});
 
 		e.preventDefault();
 	});
@@ -78,6 +84,4 @@ jQuery(document).ready(function() {
 			e.preventDefault();
 		});
 	}
-
-	
 });
